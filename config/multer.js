6b2398c2d3a02b17d6fcc82ada4d4c26f7a389a5
Aditya <aditya52_1@live.com>
2020-08-11 21:34:40 +0530

    first commit

---
+++ config/multer.js
@@ -0,0 +1,13 @@
+//uploading files or images
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination : (req, file, cb) => {
+        cb(null , "public/uploads");
+    },
+    filename: (req, file , cb) => {
+        cb(null , Date.now() + file.originalname)
+    }
+});
+
+module.exports = { storage };
